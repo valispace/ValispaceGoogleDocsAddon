3f5fdeaa84c995bd00ef2022c404500934951a51
Cauê Napier <cauenapierp@gmail.com>
2021-04-18 00:59:22 +0100

    Improved Downloading

--- src/Code.js
+++ src/Code.js
@@ -1,35 +1,37 @@
-function showSidebar() {
+var TemplateDocumentId_original = '1bDQClCWVcvzPARYl5ohGvBgZlQ519NGGCStqizzK-bU';
 
-  if (checkValispaceConnexion()){
-    var template = HtmlService.createTemplateFromFile('frontend/sidebarTemplate');
-    Logger.log('Already Connected')
-  } else {
-    var template = HtmlService.createTemplateFromFile('frontend/loginPage');
-    Logger.log('No Connection')
-  }
-  
- 
-  var page = template.evaluate();
-  page.setTitle('Valispace on Google Docs');
- 
-  DocumentApp.getUi().showSidebar(page);
-  
-  var TemplateDocumentId_original = '1bDQClCWVcvzPARYl5ohGvBgZlQ519NGGCStqizzK-bU';
+
+function onOpen(e) {
+
+  // TODO: Maybe we can start downloading data here to speed up
   if (PropertiesService.getDocumentProperties().getProperty('TemplateDocumentId') === null) {
     PropertiesService.getDocumentProperties().setProperty('TemplateDocumentId', TemplateDocumentId_original);
-  };  
-  // if (PropertiesService.getDocumentProperties().getProperty('highlightVali') === null) {
-  //   PropertiesService.getDocumentProperties().setProperty('highlightVali', false);
-  // };
-  // if (PropertiesService.getDocumentProperties().getProperty('individualReq') === null) {
-  //   PropertiesService.getDocumentProperties().setProperty('individualReq', false);
-  // };
-  // if (PropertiesService.getDocumentProperties().getProperty('firstRowHeader') === null) {
-  //   PropertiesService.getDocumentProperties().setProperty('firstRowHeader', false);
-  // };
+  };
+
+  DocumentApp.getUi().createAddonMenu()
+    .addItem('Show sidebar', 'showSidebar')
+    .addToUi();
 }
 
-function disconnect(){
+
+
+function showSidebar() {
+
+  // Check if Connection is still valid and skip login page if valid.
+  if (checkValispaceConnexion()) {
+    var template = HtmlService.createTemplateFromFile('frontend/sidebarTemplate');
+  } else {
+    var template = HtmlService.createTemplateFromFile('frontend/loginPage');
+  }
+
+  var page = template.evaluate();
+  page.setTitle('Valispace on Google Docs');
+
+  DocumentApp.getUi().showSidebar(page);
+
+}
+
+function disconnect() {
   response = PropertiesService.getUserProperties().deleteProperty('access_token');
   var template = HtmlService.createTemplateFromFile('frontend/loginPage');
   var page = template.evaluate();
@@ -38,27 +40,18 @@ function disconnect(){
   DocumentApp.getUi().showSidebar(page);
 }
 
-function goToMainPage(){
+function goToMainPage() {
   // TODO : Remove this if not necessary
   Logger.log('Is it connected?')
   Logger.log(checkValispaceConnexion())
   var template = HtmlService.createTemplateFromFile('frontend/sidebarTemplate');
   var page = template.evaluate();
   page.setTitle('Valispace on Google Docs');
- 
+
   DocumentApp.getUi().showSidebar(page);
 }
 
-function onInstall(e){
+function onInstall(e) {
   onOpen(e);
-}
-
-function onOpen(e) {
-
-  // TODO: Maybe we can start downloading data here to speed up
-  
-  DocumentApp.getUi().createAddonMenu()
-      .addItem('Show sidebar', 'showSidebar')
-      .addToUi();
 }
 
--- src/frontend/options.js
+++ src/frontend/options.js
@@ -1,21 +1,8 @@
 function openOptionDialog() { 
   var template = HtmlService.createTemplateFromFile('frontend/optionsDialog');
-
-  template.highlightOption = "";
-  
-  if (PropertiesService.getDocumentProperties().getProperty('highlightVali') == "true"){
-    template.highlightOption = "checked";
-  } 
-  else if (PropertiesService.getDocumentProperties().getProperty('highlightVali') == "false"){
-    template.highlightOption = "";
-  }
-  
   var page_options = template.evaluate();
-    
-  
   page_options.setTitle('Options');
   page_options.setHeight(200);
-  
   var dialog = DocumentApp.getUi().showModalDialog(page_options, 'Options');
 }
 
@@ -23,31 +10,14 @@ function openOptionDialog() {
 function changeReqTableTemplate(TemplateDocumentId){
   try{
     PropertiesService.getDocumentProperties().setProperty('TemplateDocumentId', TemplateDocumentId);
-    Logger.log('Changed template document to ID: ')
-    Logger.log(TemplateDocumentId)
-//    var templatedoc = DocumentApp.openById(TemplateDocumentId);
   } catch (error) {
     DocumentApp.getUi().alert("Could not find the document. Confirm it was not deleted and that anyone have read access with the link.");
-    //Logger.log("Document not accessible", TemplateDocumentId)
   } 
 }
 
 function resetReqTableTemplate(){
-  var TemplateDocumentId_original = '1bDQClCWVcvzPARYl5ohGvBgZlQ519NGGCStqizzK-bU';
   PropertiesService.getDocumentProperties().setProperty('TemplateDocumentId', TemplateDocumentId_original);
 }
-
-function changeOptions(highlightVali){
-  previousSetting = PropertiesService.getDocumentProperties().getProperty('highlightVali'); 
-  //Logger.log('Previous Settings '+previousSetting)
-  if (previousSetting != highlightVali){
-    PropertiesService.getDocumentProperties().setProperty('highlightVali', highlightVali);
-  };
-  //Logger.log('new Settings '+PropertiesService.getDocumentProperties().getProperty('highlightVali'))
-    
-  //google.script.run.valisHighlightChange(highlightVali);
-}
-
 
 function clearUserProperties(){
   var userProperties = PropertiesService.getUserProperties();
